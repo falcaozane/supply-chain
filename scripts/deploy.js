f232069ff242bd1ef42140de99a5f7c62b873120
Zane Falcao <falcaozane@gmail.com>
2023-09-14 12:54:20 +0530

    deploy contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,13 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const Tracking = await hre.ethers.getContractFactory("Tracking");
+  const tracking = await Tracking.deploy();
 
-  await lock.waitForDeployment();
+  await tracking.waitForDeployment();
 
   console.log(
-    `Tracking deployed to ${lock.target}`
+    `Tracking deployed to ${tracking.target}`
   );
 }
 
