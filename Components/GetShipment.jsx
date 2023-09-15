77f72e4bf0b4a922ae5bf094ec34ef96b552630b
Alroy <alroypereira05@gmail.com>
2023-09-15 20:01:06 +0530

    getshipment comp

--- Components/GetShipment.jsx
+++ Components/GetShipment.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SVG from '../Components/SVG/Str1';
 
-const GetShipment = () => {
-  return (
-    <div>GetShipment</div>
-  )
+export default function GetShipmentDetails({ getModel, setGetModel, getShipment }) {
+  const [index, setIndex] = useState(0);
+  const [singleShipmentData, setSingleShipmentData] = useState();
+
+  const getShipmentData = async () => {
+    const getData = await getShipment(parseInt(index)); // Parse index as an integer
+    setSingleShipmentData(getData);
+    console.log(getData);
+  };
+
+  const convertTime = (time) => {
+    const newTime = new Date(time);
+    const dateTime = new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    }).format(newTime);
+
+    return dateTime;
+  };
+
+  return getModel ? (
+    <div className="fixed inset-0 z-10 overflow-y-auto">
+      <div className='fixed inset-0 w-full h-full bg-black opacity-40' onClick={() => setGetModel(false)}></div>
+      <div className='relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg'>
+        <div className='flex justify-end'>
+          <button className='p-2 text-gray-400 rounded-md hover:bg-gray-200' onClick={() => setGetModel(false)}><SVG /></button>
+        </div>
+        <div className='max-w-sm mx-auto py-3 space-y-3 text-center'>
+          <h4 className='text-lg font-medium text-gray-800'>Product Tracking Details</h4>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className='relative mt-3'>
+              <input type="number" placeholder='ID' className='w-full pl-5 pr-3 py-5 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg' onChange={(e) => setIndex(e.target.value)} />
+            </div>
+            <button onClick={() => getShipmentData()} className='block w-full mt-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2'>
+              Get Details
+            </button>
+          </form>
+
+          {
+            singleShipmentData === undefined ? (
+              ""
+            ) : (
+              <div className='text-left'>
+                <p>Sender: {singleShipmentData.sender.slice(0, 25)}...</p>
+                <p>Receiver: {singleShipmentData.receiver.slice(0, 25)}...</p>
+                <p>Pickup Time: {convertTime(singleShipmentData.pickupTime)}</p>
+                <p>Delivery Time: {convertTime(singleShipmentData.deliveryTime)}</p>
+                <p>Distance: {singleShipmentData.distance}</p>
+                <p>Price: {singleShipmentData.price}</p>
+                <p>Status: {singleShipmentData.status}</p>
+                <p>Paid: {singleShipmentData.isPaid ? "Complete" : "Not complete"}</p>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
-
-export default GetShipment
