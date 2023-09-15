acdbc88e935cfaeab81d584742f6d39e94690248
JonathanDabre <jonathandabre01@gmail.com>
2023-09-15 20:23:19 +0530

    Bug fixes in GetShipment

--- Components/GetShipment.jsx
+++ Components/GetShipment.jsx
@@ -36,9 +36,9 @@ export default function GetShipmentDetails({ getModal, setGetModal, getShipment 
           <h4 className='text-lg font-medium text-gray-800'>Product Tracking Details</h4>
           <form onSubmit={(e) => e.preventDefault()}>
             <div className='relative mt-3'>
-              <input type="number" placeholder='ID' className='w-full pl-5 pr-3 py-5 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg' onChange={(e) => setIndex(e.target.value)} />
+              <input type="number" placeholder='ID' className='w-full pl-5 pr-3 py-3 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg' onChange={(e) => setIndex(e.target.value)} />
             </div>
-            <button onClick={() => getShipmentData()} className='block w-full mt-3 px-4 py-2 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2'>
+            <button onClick={() => getShipmentData()} className='block w-full mt-3 px-4 py-3 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg ring-offset-2 ring-indigo-600 focus:ring-2'>
               Get Details
             </button>
           </form>
